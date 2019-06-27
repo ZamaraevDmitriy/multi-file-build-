eba7816de6dbe3d50b068153ca771e67d427eaaa
Zamaraev Dmitriy <zamaraev-dmitriy@mail.ru>
2019-06-27 21:55:50 +0400

    init

---
+++ source/repos/PersonLibrary/PersonLibrary/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace PersonLibrary
+{
+    //Многофайловая сборка
+    public class Person
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+
+        public Person() { 
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+        public void AboutPerson()
+        {
+
+            MessageBox.Show("Surname -" + Surname + ", Name - " + Name + ", Age - " + Age);
+        }
+
+    }
+}
